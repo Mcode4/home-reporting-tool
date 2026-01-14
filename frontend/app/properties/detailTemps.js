537f3dfb01af88ff0ac038a167b361a63f36f2a6
Mcode4 <lamllc20@gmail.com>
2026-01-14 04:40:22 -0500

    detail Temp complete

--- frontend/app/properties/detailTemps.js
+++ frontend/app/properties/detailTemps.js
@@ -45,8 +45,6 @@ export const mainTemp = ({
     }
 }
 
-// Radio1 = ["Okay", "Repair", "Replace"]
-
 export const exteriorTemp = ({
     changeFrontLocks,
     cleanFrontDoor,
@@ -63,55 +61,645 @@ export const exteriorTemp = ({
     return {
         changeFrontLocks: {
             text: 'Change locks - front door',
-            value: changeFrontLocks // Radio1
+            value: changeFrontLocks
         },
         cleanFrontDoor: {
             text: 'Clean front door area',
-            value: cleanFrontDoor // Radio1
+            value: cleanFrontDoor
         },
         paintFrontDoor: {
             text: 'Paint front door',
-            value: paintFrontDoor // Radio1
+            value: paintFrontDoor
         },
         frontDoorLight: {
             text: 'Front door light bulb/fixture',
-            value: frontDoorLight // Radio1
+            value: frontDoorLight
         },
         doorNumber: {
             text: 'Door number installed',
-            value: doorNumber // Radio1
+            value: doorNumber
         },
         kickPlate: {
             text: 'Kick plate (if needed)',
-            value: kickPlate // Radio1
+            value: kickPlate
         },
         cleanPatio: {
             text: 'Clean patio',
-            value: cleanPatio // Radio1
+            value: cleanPatio
         },
         rails: {
             text: 'Paint/clean rails (if needed)',
-            value: rails // Radio1
+            value: rails
         },
         patioDoorLock: {
             text: 'Change patio door lock',
-            value: patioDoorLock // Radio1
+            value: patioDoorLock
         },
     }
 }
 
-export const livingRoomTemp = ({}) => {}
+export const livingRoomTemp = ({
+    switchCovers,
+    windowsBroke,
+    windowsLock,
+    windowsDontOpen,
+    windowsScreenMissing,
+    cleanACVents,
+    ceilingFan,
+    banister,
+    baseboardsCrack,
+    blindsBroke,
+    hallwayLight,
+    floorsClean,
+    paint,
+    hallCloset
+}) => {
+    
+    return {
+        switchCovers: {
+            text: 'Switch covers (painted or damaged)',
+            value: switchCovers
+        },
+        windowsBroke: {
+            text: 'Windows - broken',
+            value: windowsBroke
+        },
+        windowsLock: {
+            text: 'Windows - locks',
+            value: windowsLock
+        },
+        windowsDontOpen: {
+            text: "Windows - don't open",
+            value: windowsDontOpen
+        },
+        windowsScreenMissing: {
+            text: 'Windows - screens missing',
+            value: windowsScreenMissing
+        },
+        cleanACVents: {
+            text: 'Clean A/C vents and return',
+            value: cleanACVents
+        },
+        ceilingFan: {
+            text: 'Ceiling fan - working/clean/light bulbs',
+            value: ceilingFan
+        },
+        banister: {
+            text: 'Banister - secure/paint or cleaning needed',
+            value: banister
+        },
+        baseboardsCrack: {
+            text: 'Baseboards cracked',
+            value: baseboardsCrack
+        },
+        blindsBroke: {
+            text: 'Blinds broken',
+            value: blindsBroke
+        },
+        hallwayLight: {
+            text: 'Hallway light - fixture and bulbs',
+            value: hallwayLight
+        },
+        floorsClean: {
+            text: 'Floors - carpet/vinyl need clean or replaced',
+            value: floorsClean
+        },
+        paint: {
+            text: 'Paint - touch or full',
+            value: paint
+        },
+        hallCloset: {
+            text: 'Hall closet - shelf and door stopper',
+            value: hallCloset
+        },
+    }
+}
 
-export const bedroomTemp = ({}) => {}
+export const bedroomTemp = ({
+    doorknobLock,
+    doorDamage,
+    doorStopper,
+    closetDoorHinge,
+    closetDoorStopper,
+    closetShelves,
+    switchCovers,
+    cleanACVents,
+    lightbulbs,
+    ceilingFan,
+    windowsBroke,
+    windowsLock,
+    windowsDontOpen,
+    windowsScreenMissing,
+    baseboardsCrack,
+    blindsBroke,
+    flooringCondition,
+    closetFlooring
+}) => {
 
-export const bathTemp = ({}) => {}
+    return {
+        doorknobLock: {
+            text: 'Doorknob locks working',
+            value: doorknobLock
+        },
+        doorDamage: {
+            text: 'Door - open/close damages',
+            value: doorDamage
+        },
+        doorStopper: {
+            text: 'Door - door stopper',
+            value: doorStopper
+        },
+        closetDoorHinge: {
+            text: 'Closet door - on hinge/track',
+            value: closetDoorHinge
+        },
+        closetDoorStopper: {
+            text: 'Closet door - door stopper',
+            value: closetDoorStopper
+        },
+        closetShelves: {
+            text: 'Closet shelves - secure',
+            value: closetShelves
+        },
+        switchCovers: {
+            text: 'Switch covers - painted/damaged',
+            value: switchCovers
+        },
+        cleanACVents: {
+            text: 'Clean A/C vents',
+            value: cleanACVents
+        },
+        lightbulbs: {
+            text: 'Lightbulbs - light fixture',
+            value: lightbulbs
+        },
+        ceilingFan: {
+            text: 'Ceiling fan - working',
+            value: ceilingFan
+        },
+        windowsBroke: {
+            text: 'Windows - broken',
+            value: windowsBroke
+        },
+        windowsLock: {
+            text: 'Windows - locks',
+            value: windowsLock
+        },
+        windowsDontOpen: {
+            text: "Windows - don't open",
+            value: windowsDontOpen
+        },
+        windowsScreenMissing: {
+            text: 'Windows - screens missing',
+            value: windowsScreenMissing
+        },
+        baseboardsCrack: {
+            text: 'Baseboard - cracked',
+            value: baseboardsCrack
+        },
+        blindsBroke: {
+            text: 'Blinds - broken',
+            value: blindsBroke
+        },
+        flooringCondition: {
+            text: 'Flooring - carpet/vinyl condition',
+            value: flooringCondition
+        },
+        closetFlooring: {
+            text: 'Closet flooring',
+            value: closetFlooring
+        },
+    }
+}
 
-export const diningRoomTemp = ({}) => {}
+export const bathTemp = ({
+    flooringPresentable,
+        counterTopResurface,
+        counterTopCaulk,
+        mirrorCracked,
+        faucetDrip,
+        stopperOld,
+        medicineCabinetDamaged,
+        medicineCabinetFaded,
+        lightbulbs,
+        toiletSeat,
+        toiletWorking,
+        toiletCaulking,
+        toiletLeaking,
+        bathtubStopper,
+        bathtubResurface,
+        bathtubShowerDiverter,
+        bathtubTowelBar,
+        bathtubShowerHead,
+        bathtubCurtainRob,
+        bathroomFaucetWorking,
+        bathroomFaucetHotCold,
+        bathroomFaucetStopper,
+        bathroomTowelRacks,
+        bathroomToiletPaperHolder,
+        bathroomDoorHingesKnobsStrikePlates,
+        bathroomShelvingIntact,
+        fartFan,
+        bathroomBaseboardsDamage,
+}) => {
 
-export const kitchenTemp = ({}) => {}
+    return {
+        flooringPresentable: {
+            text: 'Flooring - vinyl/tile presentable',
+            value: flooringPresentable
+        },
+        counterTopResurface: {
+            text: 'Countertop - resurface',
+            value: counterTopResurface
+        },
+        counterTopCaulk: {
+            text: 'Countertop caulk',
+            value: counterTopCaulk
+        },
+        mirrorCracked: {
+            text: 'Mirror - cracked/discolored',
+            value: mirrorCracked
+        },
+        faucetDrip: {
+            text: 'Faucet - dripping/leaking',
+            value: faucetDrip
+        },
+        stopperOld: {
+            text: 'Stopper - remove old system',
+            value: stopperOld
+        },
+        medicineCabinetDamaged: {
+            text: 'Medicine cabinet - clean/damaged',
+            value: medicineCabinetDamaged
+        },
+        medicineCabinetFaded: {
+            text: 'Medicine cabinet - mirror faded spots',
+            value: medicineCabinetFaded
+        },
+        lightbulbs: {
+            text: 'Lightbulbs - light fixture secure',
+            value: lightbulbs
+        },
+        toiletSeat: {
+            text: 'Toilet - seat',
+            value: toiletSeat
+        },
+        toiletWorking: {
+            text: 'Toilet - working with water',
+            value: toiletWorking
+        },
+        toiletCaulking: {
+            text: 'Toilet - bolts/caulking',
+            value: toiletCaulking
+        },
+        toiletLeaking: {
+            text: 'Toilet - running/leaking',
+            value: toiletLeaking
+        },
+        bathtubStopper: {
+            text: 'Bathtub - stopper',
+            value: bathtubStopper
+        },
+        bathtubResurface: {
+            text: 'Bathtub - resurfacing needed',
+            value: bathtubResurface
+        },
+        bathtubShowerDiverter: {
+            text: 'Bathtub - shower diverter working/no leaks',
+            value: bathtubShowerDiverter
+        },
+        bathtubTowelBar: {
+            text: 'Bathtub - shower towel bar',
+            value: bathtubTowelBar
+        },
+        bathtubShowerHead: {
+            text: 'Bathtub - shower head',
+            value: bathtubShowerHead
+        },
+        bathtubCurtainRob: {
+            text: 'Bathtub - shower curtain rod',
+            value: bathtubCurtainRob
+        },
+        bathroomFaucetWorking: {
+            text: 'Bathroom faucet - working no leaks',
+            value: bathroomFaucetWorking
+        },
+        bathroomFaucetHotCold: {
+            text: 'Bathroom faucet - hot/cold water',
+            value: bathroomFaucetHotCold
+        },
+        bathroomFaucetStopper: {
+            text: 'Bathroom faucet - stopper missing',
+            value: bathroomFaucetStopper
+        },
+        bathroomTowelRacks: {
+            text: 'Bathroom - towel racks',
+            value: bathroomTowelRacks
+        },
+        bathroomToiletPaperHolder: {
+            text: 'Bathroom - toilet paper holder',
+            value: bathroomToiletPaperHolder
+        },
+        bathroomDoorHingesKnobsStrikePlates: {
+            text: 'Bathroom - door/hinges/knobs/strike plates',
+            value: bathroomDoorHingesKnobsStrikePlates
+        },
+        bathroomShelvingIntact: {
+            text: 'Bathroom - shelving intact',
+            value: bathroomShelvingIntact
+        },
+        fartFan: {
+            text: 'Fart fan - working, clean',
+            value: fartFan
+        },
+        bathroomBaseboardsDamage: {
+            text: 'Bathroom - baseboards cracked/water damaged',
+            value: bathroomBaseboardsDamage
+        },
+    }
+}
 
-export const laundryRoomTemp = ({}) => {}
+export const diningRoomTemp = ({
+    lightFixture,
+    switchPlatesDamage,
+    flooringCondition,
+}) => {
 
-export const patioTemp = ({}) => {}
+    return {
+        lightFixture: {
+            text: "Light fixture - bulbs working/secure",
+            value: lightFixture
+        },
+        switchPlatesDamage: {
+            text: "Switch plates - painted/damaged",
+            value: switchPlatesDamage
+        },
+        flooringCondition: {
+            text: "Flooring - vinyl/carpet condition",
+            value: flooringCondition
+        },
+    }
+}
 
-export const smokeDetectorTemp = ({}) => {}
+export const kitchenTemp = ({
+    flooringDamage,
+    counterTopResurface,
+    counterTopCaulk,
+    switchPlatesDamage,
+    kitchenSinkFireExtinguisher,
+    kitchenSinkStopper,
+    kitchenSinkFaucetDrip,
+    kitchenSinkFaucetSprayer,
+    garbageDisposal,
+    cabinetsInside,
+    cabinetsDoorDrawers,
+    cabinetDoorKnob,
+    kitchenDrawersBroke,
+    cabinetsShelvesKnobs,
+    cabinetTop,
+    stoveWorking,
+    stoveDripPan,
+    stoveKnobOven,
+    stoveDoorBottomTray,
+    microwaveWorking,
+    microwaveFilterFireSuppression,
+    microwaveLight,
+    refrigeratorNoise,
+    refrigeratorIceMaker,
+    refrigeratorLightShelvesCracks,
+    refrigeratorTrash,
+    dishwasherCycle,
+    kitchenVents,
+}) => {
+
+    return {
+        flooringDamage: {
+            text: 'Flooring - vinyl/tile damaged',
+            value: flooringDamage
+        },
+        counterTopResurface: {
+            text: 'Countertop - resurfacing needed',
+            value: counterTopResurface
+        },
+        counterTopCaulk: {
+            text: 'Countertop - caulking',
+            value: counterTopCaulk
+        },
+        switchPlatesDamage: {
+            text: 'Switch plates - painted/damaged',
+            value: switchPlatesDamage
+        },
+        kitchenSinkFireExtinguisher: {
+            text: 'Kitchen sink - fire extinguisher full/expired',
+            value: kitchenSinkFireExtinguisher
+        },
+        kitchenSinkStopper: {
+            text: 'Kitchen sink - stoppers',
+            value: kitchenSinkStopper
+        },
+        kitchenSinkFaucetDrip: {
+            text: 'Kitchen sink faucet - leaking/dripping',
+            value: kitchenSinkFaucetDrip
+        },
+        kitchenSinkFaucetSprayer: {
+            text: 'Kitchen sink - faucet sprayer/secure',
+            value: kitchenSinkFaucetSprayer
+        },
+        garbageDisposal: {
+            text: 'Garbage disposal - working/clean',
+            value: garbageDisposal
+        },
+        cabinetsInside: {
+            text: 'Cabinets - inside clean',
+            value: cabinetsInside
+        },
+        cabinetsDoorDrawers: {
+            text: 'Cabinets - doors/drawers secure',
+            value: cabinetsDoorDrawers
+        },
+        cabinetDoorKnob: {
+            text: 'Cabinets - doors/knobs',
+            value: cabinetDoorKnob
+        },
+        kitchenDrawersBroke: {
+            text: 'Kitchen drawers - on track/broken',
+            value: kitchenDrawersBroke
+        },
+        cabinetsShelvesKnobs: {
+            text: 'Cabinets - shelves/knobs secure/missing',
+            value: cabinetsShelvesKnobs
+        },
+        cabinetTop: {
+            text: 'Cabinet tops - clean',
+            value: cabinetTop
+        },
+        stoveWorking: {
+            text: 'Stove - working/clean',
+            value: stoveWorking
+        },
+        stoveDripPan: {
+            text: 'Stove - drip pans/eyes missing',
+            value: stoveDripPan
+        },
+        stoveKnobOven: {
+            text: 'Stove - knobs/oven light',
+            value: stoveKnobOven
+        },
+        stoveDoorBottomTray: {
+            text: 'Stove - door/bottom tray/on track',
+            value: stoveDoorBottomTray
+        },
+        microwaveWorking: {
+            text: 'Microwave - working/clean',
+            value: microwaveWorking
+        },
+        microwaveFilterFireSuppression: {
+            text: 'Microwave - filter/fire suppression',
+            value: microwaveFilterFireSuppression
+        },
+        microwaveLight: {
+            text: 'Microwave - light',
+            value: microwaveLight
+        },
+        refrigeratorNoise: {
+            text: 'Refrigerator - loud noise',
+            value: refrigeratorNoise
+        },
+        refrigeratorIceMaker: {
+            text: 'Refrigerator - ice maker working',
+            value: refrigeratorNoise
+        },
+        refrigeratorLightShelvesCracks: {
+            text: 'Refrigerator - lightbulbs/shelves/cracks',
+            value: refrigeratorLightShelvesCracks
+        },
+        refrigeratorTrash: {
+            text: 'Refrigerator - trash out',
+            value: refrigeratorTrash
+        },
+        dishwasherCycle: {
+            text: 'Dishwasher - run cycle/secure',
+            value: dishwasherCycle
+        },
+        kitchenVents: {
+            text: 'Kitchen vents - clean',
+            value: kitchenVents
+        },
+    }
+}
+
+export const laundryRoomTemp = ({
+    flooringDamage,
+    washerCycleLeaksNoise,
+    dryerCycleLeaksNoise,
+    shelves,
+    circuitBreakerBox,
+    laundryMachine,
+    lightFixture,
+    baseboardsCrack,
+}) => {
+
+    return {
+        flooringDamage: {
+            text: 'Flooring - vinyl/carpet damaged',
+            value: flooringDamage
+        },
+        washerCycleLeaksNoise: {
+            text: 'Washer - run cycle/leaks/loud noises',
+            value: washerCycleLeaksNoise
+        },
+        dryerCycleLeaksNoise: {
+            text: 'Dryer - run cycle/leaks/loud noises',
+            value: dryerCycleLeaksNoise
+        },
+        shelves: {
+            text: 'Shelves - secure',
+            value: shelves
+        },
+        circuitBreakerBox: {
+            text: 'Circuit breaker box - secure',
+            value: circuitBreakerBox
+        },
+        laundryMachine: {
+            text: 'Laundry machine - area clean',
+            value: laundryMachine
+        },
+        lightFixture: {
+            text: 'Light fixture - lightbulbs',
+            value: lightFixture
+        },
+        baseboardsCrack: {
+            text: 'Baseboards - cracked/water damaged',
+            value: baseboardsCrack
+        },
+    }
+}
+
+export const patioTemp = ({
+    flooringDamage,
+    railing,
+    lightFixture,
+    pestControl,
+    hotWaterHeater,
+    heatACUnitWorking,
+    heatACUnitThermostat,
+    HVACRoomDamage,
+    HVACRoomDoorHingeLock,
+}) => {
+
+    return {
+        flooringDamage: {
+            text: 'Flooring - cracked/damaged',
+            value: flooringDamage
+        },
+        railing: {
+            text: 'Railings - secure/paint or cleaning needed',
+            value: railing
+        },
+        lightFixture: {
+            text: 'Light fixture - bulbs working/secure',
+            value: lightFixture
+        },
+        pestControl: {
+            text: 'Pest control - wasp/ants/spiders',
+            value: pestControl
+        },
+        hotWaterHeater: {
+            text: 'Hot water heater - leaks/rust',
+            value: hotWaterHeater
+        },
+        heatACUnitWorking: {
+            text: 'Heat & AC unit - working properly',
+            value: heatACUnitWorking
+        },
+        heatACUnitThermostat: {
+            text: 'Heat & AC unit - filter/thermostat powered',
+            value: heatACUnitThermostat
+        },
+        HVACRoomDamage: {
+            text: 'HVAC room - no dust/mildew/water damage',
+            value: HVACRoomDamage
+        },
+        HVACRoomDoorHingeLock: {
+            text: 'HVAC room - doors/hinges/locks',
+            value: HVACRoomDoorHingeLock
+        },
+    }
+}
+
+export const smokeDetectorTemp = ({
+    detectorBattery,
+    detectorReplaced,
+}) => {
+
+    return {
+        detectorBattery: {
+            text: 'Detectors - batteries changed',
+            value: detectorBattery
+        },
+        detectorReplaced: {
+            text: 'Detectors - replaced',
+            value: detectorReplaced
+        },
+    }
+}

c598ed48c0dd62e368860253c897d9ce2bfc21f1
Mcode4 <lamllc20@gmail.com>
2026-01-14 13:49:59 -0500

    home page database integration

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,29 +1,43 @@
 "use client";
 import { useRouter } from "next/navigation"
+import { useState, useEffect } from "react";
+import { getAllProperties } from "@/api/property";
 import styles from './page.module.css'
 
 export default function HomePagePage() {
     const router = useRouter();
-    const data = [{id: 1, name: 'Initial Property'}, {id: 2, name: 'Secondary Property'}]
+    const [data, setData] = useState(null);
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(()=> {
+        if(!data) {
+            const properties = getAllProperties()
+                .then((prop)=> setData(prop["properties"]))
+                .then(()=> setLoaded(true))
+                .catch((e)=> console.log('Error occured: ', e))
+        }
+    }, [data])
 
     return (
         <div className="defaultWrapper">
-        <button onClick={()=> router.push('/properties/new')}>Create New Property</button>
+        {loaded && (
+            <>
+            <button onClick={()=> router.push('/properties/new')}>Create New Property</button>
 
-        <h2>Properties</h2>
-        <div className={styles.properySection}>
-            
-            {data.length > 0 ? data.map(property => (
-                <div className={styles.property} key={property.id}>
-                    <img src="" alt={`Property${property.id}`} />
-                    <div>{property.name}</div>
-                </div>
-            )) : (
-                <p>Must a property</p>
-            )}
-        </div>
-
-        
+            <h2>Properties</h2>
+            <div className={styles.properySection}>
+                
+                {data?.length > 0 ? data.map(property => (
+                    <div className={styles.property} key={property.id}>
+                        <img src="" alt={`Property${property.id}`} />
+                        <div>{property.name}</div>
+                    </div>
+                )) : (
+                    <p>Must a property</p>
+                )}
+            </div>
+            </>
+        )}
         </div>
     )
 }
